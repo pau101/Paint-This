eb6777d6a067224665a1bbbc31310782c7109005
pau101 <paul.fulham0@gmail.com>
2016-03-29 13:02:46 -0700

    Release 0.0.2

--- src/main/java/com/pau101/paintthis/PaintThis.java
+++ src/main/java/com/pau101/paintthis/PaintThis.java
@@ -29,7 +29,7 @@ public class PaintThis {
 
 	public static final String NAME = "Paint This!";
 
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 
 	public static final int MAX_CANVAS_SIZE = 4;
 
